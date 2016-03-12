bc90a5c165e0a7afadd4d1fd1a8ed1ec3b1a127d
megapixel23 <megapixel23@ya.ru>
2016-03-12 16:24:17 +0200

    Dev constants were added.

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=resizer',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=heroku_84b4dbbd7682be8',
+    'username' => 'bae2da9d0b24ab',
+    'password' => '81444624',
     'charset' => 'utf8',
 ];
