f48e3b5cb536c1dffb95b1f65cf5ec0d609cb93a
megapixel23 <megapixel23@ya.ru>
2016-03-12 16:50:08 +0200

    Dev constants were added.

--- config/db.php
+++ config/db.php
@@ -2,16 +2,15 @@
 
 $url = parse_url(getenv('CLEARDB_DATABASE_URL'));
 
+$scheme = isset($url['scheme']) ? $url['scheme'] : 'mysql';
 $server = isset($url['host']) ? $url['host'] : 'localhost';
 $username = isset($url['user']) ? $url['user'] : 'root';
 $password = isset($url['pass']) ? $url['pass'] : '';
 $db = isset($url['path']) && substr($url['path'], 1) ? substr($url['path'], 1) : 'resizer';
 
-
-echo "<pre>"; print_r($url); echo "</pre>"; die();
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => "mysqli:host={$server};dbname={$db}",
+    'dsn' => "{$scheme}:host={$server};dbname={$db}",
     'username' => $username,
     'password' => $password,
     'charset' => 'utf8',
