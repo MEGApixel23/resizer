7aa48f141d1658af405652216ac9b092203a6dba
megapixel23 <megapixel23@ya.ru>
2016-03-12 16:48:32 +0200

    Dev constants were added.

--- config/db.php
+++ config/db.php
@@ -7,9 +7,11 @@ $username = isset($url['user']) ? $url['user'] : 'root';
 $password = isset($url['pass']) ? $url['pass'] : '';
 $db = isset($url['path']) && substr($url['path'], 1) ? substr($url['path'], 1) : 'resizer';
 
+
+echo "<pre>"; print_r($url); echo "</pre>"; die();
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => "mysql:host={$server};dbname={$db}",
+    'dsn' => "mysqli:host={$server};dbname={$db}",
     'username' => $username,
     'password' => $password,
     'charset' => 'utf8',
